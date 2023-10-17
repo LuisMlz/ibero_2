abd5772a9517be78eab44335253b972a693a8a9f
lmelendez <luis.melendez@iberoleon.mx>
2023-10-17 11:09:24 -0600

    SITIO FINAL

--- serviceWorker.js
+++ serviceWorker.js
@@ -21,3 +21,15 @@ self.addEventListener("fetch", fetchEvent => {
     })
   );
 });
+
+//Agregamos notificaciones push
+self.addEventListener('push', function(event) {
+  const options = {
+    body: event.data.text(),
+    // icon: 'icon.png',
+  };
+
+  event.waitUntil(
+    self.registration.showNotification('Push Notification', options)
+  );
+});
