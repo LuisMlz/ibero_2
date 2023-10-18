c327606a2f265d6fe531fb830d89a8eb35738097
lmelendez <luis.melendez@iberoleon.mx>
2023-10-18 13:59:22 -0600

    VERSIÓN ESTABLE 1.0

--- js/login.js
+++ js/login.js
@@ -20,12 +20,11 @@ document.addEventListener("DOMContentLoaded", function() {
     const splash = document.getElementById('splash');
     const container = document.querySelector(".divVCard");
     const userInput = document.getElementById('user');
-    const isInStandaloneMode = window.matchMedia('(display-mode: fullscreen)').matches;
+    const isInStandaloneMode = window.matchMedia('(display-mode: standalone)').matches;
     const soExcluidos = ["Windows", "macOS", "Linux","Desconocido",];
 
     //SPLASH DE INICIO
     if(!soExcluidos.includes(detectarSistemaOperativo())){
-        alert(detectarSistemaOperativo())
         //VALIDAMOS SI SE ABRIO EN SAFARI O YA ESTA INSTALADO
         if (isInStandaloneMode) {
             splash.style.display = "flex";
