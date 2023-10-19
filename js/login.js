bf8cd8c67f730a181152708f0dfdcb5a9e06d670
lmelendez <luis.melendez@iberoleon.mx>
2023-10-19 15:40:07 -0600

    VERSIÓN ESTABLE 1.0

--- js/login.js
+++ js/login.js
@@ -16,12 +16,14 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     
-    var soIncluidos = ["Windows"];
+    var soIncluidos = ["iOS"];
     
     //SPLASH DE INICIO
     if(soIncluidos.includes(detectarSistemaOperativo())){
         //VALIDAMOS SI SE ABRIO EN SAFARI O YA ESTA INSTALADO
-        var splash = document.getElementById('splash');
+        var isInStandaloneMode = window.matchMedia('(display-mode: standalone)').matches;
+        if (isInStandaloneMode) {
+            var splash = document.getElementById('splash');
             splash.style.display = "flex";
             setTimeout(function() {
                 splash.style.opacity = '0';
@@ -29,6 +31,8 @@ document.addEventListener("DOMContentLoaded", function() {
                     splash.style.display = 'none';
                 }, 1000);
             }, 2000);
+        }
+
     }
 
     //VARIABLES GLOBALES
