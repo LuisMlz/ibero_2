b5723cb88765a50febcd971ca3711bbed19c9abe
lmelendez <luis.melendez@iberoleon.mx>
2023-10-19 14:54:39 -0600

    VERSIÓN ESTABLE 1.0

--- js/login.js
+++ js/login.js
@@ -16,14 +16,12 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     
-    var soIncluidos = ["iOS"];
+    var soIncluidos = ["Windows"];
     
     //SPLASH DE INICIO
     if(soIncluidos.includes(detectarSistemaOperativo())){
         //VALIDAMOS SI SE ABRIO EN SAFARI O YA ESTA INSTALADO
-        var isInStandaloneMode = window.matchMedia('(display-mode: standalone)').matches;
-        if (isInStandaloneMode) {
-            var splash = document.getElementById('splash');
+        var splash = document.getElementById('splash');
             splash.style.display = "flex";
             setTimeout(function() {
                 splash.style.opacity = '0';
@@ -31,8 +29,6 @@ document.addEventListener("DOMContentLoaded", function() {
                     splash.style.display = 'none';
                 }, 1000);
             }, 2000);
-        }
-
     }
 
     //VARIABLES GLOBALES
--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 1.1;
+const CACHE_VERSION = 1.2;
 const CACHE_NAME = `vcard-cache-v${CACHE_VERSION}`;
 
 const assets = [
@@ -35,17 +35,13 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    // Intenta cargar la solicitud desde la red
     fetch(event.request)
       .then(function(response) {
-        // Si se carga correctamente desde la red, respondemos con la respuesta de la red
         console.log("CARGO CON RED")
         return response;
       })
       .catch(function() {
-        // Si falla la carga desde la red, intentamos cargar desde la caché
         return caches.match(event.request);
-        console.log("CARGO CON CACHE")
       })
   );
 });
