d88e0d036600b14fec6893b4929137894a08e2a8
lmelendez <luis.melendez@iberoleon.mx>
2023-10-19 10:02:56 -0600

    VERSIÓN ESTABLE 1.0

--- js/login.js
+++ js/login.js
@@ -8,18 +8,16 @@
     --Notes:        En IOS se han tenido problemas de compatibilidad en Android al
                     parecer todo bien.
 */
-const CACHE_VERSION = 1;
+const CACHE_VERSION = 2;
 const CACHE_NAME = `vcard-cache-v${CACHE_VERSION}`;
 
 document.addEventListener("DOMContentLoaded", function() {
     
-    var soIncluidos = ["iOS"];
+    var soIncluidos = ["Windows"];
     //SPLASH DE INICIO
     if(soIncluidos.includes(detectarSistemaOperativo())){
         //VALIDAMOS SI SE ABRIO EN SAFARI O YA ESTA INSTALADO
-        var isInStandaloneMode = window.matchMedia('(display-mode: standalone)').matches;
-        if (isInStandaloneMode) {
-            var splash = document.getElementById('splash');
+        var splash = document.getElementById('splash');
             splash.style.display = "flex";
             setTimeout(function() {
                 splash.style.opacity = '0';
@@ -27,7 +25,6 @@ document.addEventListener("DOMContentLoaded", function() {
                     splash.style.display = 'none';
                 }, 1000);
             }, 2000);
-        }
 
     }
 
