0a766247e90ebc3d468819c1ee6a4f96bdbdd5f8
lmelendez <luis.melendez@iberoleon.mx>
2023-10-19 10:11:18 -0600

    VERSIÓN ESTABLE 1.0

--- js/login.js
+++ js/login.js
@@ -8,16 +8,18 @@
     --Notes:        En IOS se han tenido problemas de compatibilidad en Android al
                     parecer todo bien.
 */
-const CACHE_VERSION = 2;
+const CACHE_VERSION = 3;
 const CACHE_NAME = `vcard-cache-v${CACHE_VERSION}`;
 
 document.addEventListener("DOMContentLoaded", function() {
     
-    var soIncluidos = ["Windows"];
+    var soIncluidos = ["iOS"];
     //SPLASH DE INICIO
     if(soIncluidos.includes(detectarSistemaOperativo())){
         //VALIDAMOS SI SE ABRIO EN SAFARI O YA ESTA INSTALADO
-        var splash = document.getElementById('splash');
+        var isInStandaloneMode = window.matchMedia('(display-mode: standalone)').matches;
+        if (isInStandaloneMode) {
+            var splash = document.getElementById('splash');
             splash.style.display = "flex";
             setTimeout(function() {
                 splash.style.opacity = '0';
@@ -25,6 +27,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     splash.style.display = 'none';
                 }, 1000);
             }, 2000);
+        }
 
     }
 
@@ -411,7 +414,7 @@ document.addEventListener("DOMContentLoaded", function() {
 // }
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/serviceWorker.js', { scope: '/' + CACHE_NAME })
+    navigator.serviceWorker.register('./serviceWorker.js', { scope: '/' + CACHE_NAME })
       .then((registration) => {
         console.log('Service Worker registrado con éxito.', registration.scope);
       })
